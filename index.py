975bec39e2de339bd103a2b4342b241b84d68319
Maurgene GHEZAL <maurgene.ghezal@laplateforme.io>
2020-10-13 08:13:48 +0200

    test

--- index.py
+++ index.py
@@ -16,6 +16,7 @@ app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
     html.Div(id='page-content'),
 ])
+server = app.server
 
 @app.callback(Output('page-content', 'children'),
               [Input('url', 'pathname')])
